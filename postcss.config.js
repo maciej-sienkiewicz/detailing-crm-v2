625e9544c786ab376e80f5f287491d77f14a2844
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-01-22 20:58:59 +0100

    Revert "fix: Dodaj rgb() fallbacki dla oklch() kolorów Tailwind v4"
    This reverts commit 9ee7fad8dc7c78fa8f9614e80a09e9da4eaa7829.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
-    // Convert oklch() colors to rgb() with fallbacks for older browsers
-    '@csstools/postcss-oklab-function': { preserve: true },
     autoprefixer: {},
   },
 }
