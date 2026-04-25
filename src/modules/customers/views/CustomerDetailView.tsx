0251e1585d19b3bb896816827f57551faeca84e2
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-04-25 13:33:52 +0200

    fix

--- src/modules/customers/views/CustomerDetailView.tsx
+++ src/modules/customers/views/CustomerDetailView.tsx
@@ -348,39 +348,6 @@ export const CustomerDetailView = () => {
                             </PanelBodyFlush>
                         </Panel>
 
-                        {/* Preferences */}
-                        <Panel>
-                            <PanelHead>
-                                <PanelTitle>
-                                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/>
-                                    </svg>
-                                    Preferencje
-                                </PanelTitle>
-                            </PanelHead>
-                            <PanelBody>
-                                <PrefRow>
-                                    <PrefKey>Kontakt</PrefKey>
-                                    <PrefVal>{deriveContactPreference(marketingConsents)}</PrefVal>
-                                </PrefRow>
-                                {customer.homeAddress && (
-                                    <PrefRow>
-                                        <PrefKey>Miasto</PrefKey>
-                                        <PrefVal>{customer.homeAddress.city}</PrefVal>
-                                    </PrefRow>
-                                )}
-                                <PrefRow>
-                                    <PrefKey>Typ klienta</PrefKey>
-                                    <PrefVal>{customer.company ? 'Firma' : 'Osoba prywatna'}</PrefVal>
-                                </PrefRow>
-                                <PrefRow>
-                                    <PrefKey>Marketing</PrefKey>
-                                    <PrefVal $positive={marketingConsents.some(c => c.granted)}>
-                                        {marketingConsents.some(c => c.granted) ? 'Zgoda' : 'Brak zgody'}
-                                    </PrefVal>
-                                </PrefRow>
-                            </PanelBody>
-                        </Panel>
 
                         {/* Notes */}
                         <CustomerNotes customerId={customerId!} />
