d3eecc2e382bfc96f2bdf3b41995241f2f8bdf07
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-05-26 00:47:29 +0200

    fix

--- src/modules/checkin/components/VerificationStep.tsx
+++ src/modules/checkin/components/VerificationStep.tsx
@@ -1396,9 +1396,6 @@ export const VerificationStep = ({
                     <SectionTitleRow>
                         <SectionNum>3</SectionNum>
                         <SectionLabel>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
-                            </svg>
                             {t.checkin.verification.vehicleSection}
                             {(vehicleChoiceMade || formData.isNewVehicle) && vehicleBadge && (
                                 <StatusPill>{vehicleBadge}</StatusPill>
@@ -1490,9 +1487,6 @@ export const VerificationStep = ({
                         <SectionTitleRow>
                             <SectionNum>4</SectionNum>
                             <SectionLabel>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-3-3v6m8-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
                                 Depozyt
                             </SectionLabel>
                         </SectionTitleRow>
