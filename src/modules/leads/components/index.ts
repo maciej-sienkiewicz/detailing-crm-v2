1739b301ac22d9d9a15f3f2d91f30691515b5077
MaciejSienkiewicz <kontakt@sienkiewicz-maciej.pl>
2026-07-04 11:39:37 +0200

    clkean

--- src/modules/leads/components/index.ts
+++ src/modules/leads/components/index.ts
@@ -1,6 +1,5 @@
 // src/modules/leads/components/index.ts
 export * from './LeadInbox';
 export * from './LeadForm';
-export * from './LeadTable';
 export * from './LeadStatsBar';
 export * from './PipelineSummaryWidget';
